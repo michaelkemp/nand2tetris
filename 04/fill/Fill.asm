7b8a928c3bee413749f808f13e66b02bc6536589
michaelkemp <michael.5kemps@gmail.com>
2023-10-27 12:38:42 -0600

    touch project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,62 +11,59 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+    // Check for keypress
+    (CHECKKBD)
+        @KBD
+        D=M         // id D == 0, no key is pressed
+        @NOKEY
+        D;JEQ       // JMP to lable NOKEY if 0 in KBD memory
 
-// Check for keypress
-(CHECKKBD)
-@KBD
-D=M         // id D == 0, no key is pressed
-@NOKEY
-D;JEQ 
+    (KEY)           // a key was pressed, set R0 to -1 (paint black)
+        @R0
+        M=-1
+        @PAINT
+        0;JMP       // unconditional JMP to label PAINT
 
-(KEY)       // a key was pressed, set R0 to -1 (paint black)
-@R0
-M=-1
-@PAINT
-0;JMP
+    (NOKEY)         // no key pressed, set R0 to 0 (paint white)
+        @R0
+        M=0
 
-(NOKEY)     // no key pressed, set R0 to 0 (paint white)
-@R0
-M=0
+    // put BEGIN screen address into screenBEG
+    (PAINT)
+        @SCREEN
+        D=A
+        @screenBEG
+        M=D
 
-(PAINT)
-// put BEGIN screen address into screenBEG
-@SCREEN
-D=A
-@screenBEG
-M=D
+        // put END screen address into screenEND
+        @SCREEN
+        D=A
+        @8192
+        D=D+A
+        @screenEND
+        M=D
 
-// put END screen address into screenEND
-@SCREEN
-D=A
-@8192
-D=D+A
-@screenEND
-M=D
+        // load screenBEG into upto
+        @screenBEG
+        D=M
+        @upto
+        M=D
 
-// load screenBEG into R0
-@screenBEG
-D=M
-@upto
-M=D
+    (COLOR)
+        @R0         // Get color from R0
+        D=M         // Screen Color is in D
+        @upto       // get screen location
+        A=M         // Screen Location is in A (RAM[Screen Location] is selected)
+        M=D         // set screen location color
+        @upto       // increment screen location
+        M=M+1
 
-(COLOR)
-@R0         // Get color from R0
-D=M
-@upto       // get screen location
-A=M     
-M=D         // set screen location color
-@upto       // increment screen location
-M=M+1
+        @upto       // if upto == screenEND finish the loop
+        D=M         // current "upto" in D
+        @screenEND  // "screenEnd" in M
+        D=D-M       // if D-M == 0, JMP to label CHECKKBD
+        @CHECKKBD
+        D;JEQ
 
-@upto       // if upto == screenEND finish the loop
-D=M
-@screenEND  
-D=D-M
-@CHECKKBD
-D;JEQ
-
-@COLOR
-0;JMP
-
+        @COLOR
+        0;JMP       // unconditional JMP to label COLOR
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,33 +6,31 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @R2
+    M=0     // Zero R2
 
-@R2
-M=0     // Zero R2
+    @R0
+    D=M
+    @END
+    D;JEQ   // END IF R0 == 0
 
-@R0
-D=M
-@END
-D;JEQ   // END IF R0 == 0
+    @R1
+    D=M
+    @END
+    D;JEQ   // END IF R1 == 0
 
-@R1
-D=M
-@END
-D;JEQ   // END IF R1 == 0
+    (LOOP)
+        @R0
+        D=M     // put value of R0 in D
+        @R2
+        M=D+M   // Add R2 = R2 + D
+        @R1
+        MD=M-1  // Reduce R1 by 1
+        @END
+        D;JEQ   // Goto END if R1 is 0
+        @LOOP
+        0;JMP   // Goto LOOP
 
-(LOOP)
-@R0
-D=M     // put value of R0 in D
-@R2
-M=D+M   // Add R2 = R2 + D
-@R1
-MD=M-1  // Reduce R1 by 1
-@END
-D;JEQ   // Goto END if R1 is 0
-@LOOP
-0;JMP   // Goto LOOP
-
-(END)
-@END
-0;JMP
+    (END)
+        @END
+        0;JMP
